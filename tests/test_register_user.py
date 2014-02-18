0d989a5c40c9968126b693190bc62ed3f8686fc2
Ruben Gonzalez Alonso <rga@tid.es>
2014-02-18 15:49:33 +0100

    Add copyright headers

--- tests/test_register_user.py
+++ tests/test_register_user.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
+'''
+(c) Copyright 2014 Telefonica, I+D. Printed in Spain (Europe). All Rights
+Reserved.
+
+The copyright to the software program(s) is property of Telefonica I+D.
+The program(s) may be used and or copied only with the express written
+consent of Telefonica I+D or in accordance with the terms and conditions
+stipulated in the agreement/contract under which the program(s) have
+been supplied.
+'''
 from tests.common.selenium_test_case import SeleniumTestCase
 from pageobjects.register import RegisterPageObject
 from pageobjects.register_result import RegisterResultPageObject
