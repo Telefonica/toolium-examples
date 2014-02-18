081bf186cd681c885fe7ee325ba86947262610cd
Ruben Gonzalez Alonso <rga@tid.es>
2014-02-18 16:44:04 +0100

    Run tests in Firefox, Chrome, Explorer or Phantom

--- pageobjects/common/selenium_wrapper.py
+++ pageobjects/common/selenium_wrapper.py
@@ -16,6 +16,7 @@ import os
 config = ConfigParser.ConfigParser()
 config.read('properties.cfg')
 
+
 class SeleniumWrapper(object):
     # singleton
     _instance = None
@@ -26,7 +27,58 @@ class SeleniumWrapper(object):
         return cls._instance
 
     def connect(self):
-        print config.get('Selenium', 'browser')
-        self.driver = webdriver.Firefox()
+        """
+        Set up the browser driver
+        """
+        def unknown_driver():
+            assert False, 'Unknown driver {0}'.format(config.get('Browser', 'browser'))
+
+        browser_config = {'firefox': self._setup_firefox,
+                          'chrome': self._setup_chrome,
+                          'iexplore': self._setup_explorer,
+                          'phantomjs': self._setup_phantomjs}
+
+        browser_config.get(config.get('Browser', 'browser'), unknown_driver)()
         return self.driver
-    
+
+    def _setup_firefox(self):
+        """
+        Setup Firefox webdriver
+        """
+        try:
+            profile = webdriver.FirefoxProfile()
+            profile.native_events_enabled = True
+            self.driver = webdriver.Firefox(firefox_profile=profile)
+        except:
+            assert False, "Firefox driver can not be launched."
+
+    def _setup_chrome(self):
+        """
+        Setup Chrome webdriver
+        """
+        options = webdriver.ChromeOptions()
+        try:
+            chromedriver = config.get('Browser', 'chromedriver_path')
+            self.driver = webdriver.Chrome(chromedriver, chrome_options=options)
+        except:
+            assert False, "Chrome driver can not be launched. Path given in properties: %s " % (chromedriver)
+
+    def _setup_explorer(self):
+        """
+        Setup Internet Explorer webdriver
+        """
+        try:
+            explorerdriver = config.get('Browser', 'explorerdriver_path')
+            self.driver = webdriver.Ie(explorerdriver)
+        except:
+            assert False, "Explorer driver can not be launched. Path given in properties: %s " % (explorerdriver)
+
+    def _setup_phantomjs(self):
+        """
+        Setup phantomjs webdriver
+        """
+        try:
+            phantomdriver = config.get('Browser', 'phantomdriver_path')
+            self.driver = webdriver.PhantomJS(executable_path=phantomdriver)
+        except:
+            assert False, "Phantom driver can not be launched. Path given in properties: %s " % (phantomdriver)
