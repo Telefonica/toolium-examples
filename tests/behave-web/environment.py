07ca55b74c6ce16e6a00618fe873aaee280f1f1a
Rubén González Alonso <ruben.gonzalezalonso@telefonica.com>
2015-12-21 18:41:32 +0100

    Add a new behave test over an Android app

--- tests/behave-web/environment.py
+++ tests/behave-web/environment.py
@@ -18,17 +18,48 @@ limitations under the License.
 
 import os
 
-from toolium.behave.environment import before_all as toolium_before_all, before_scenario, after_scenario, after_all
+from toolium.behave.environment import (before_all as toolium_before_all, before_scenario as toolium_before_scenario,
+                                        after_scenario as toolium_after_scenario, after_all as toolium_after_all)
 from toolium.config_files import ConfigFiles
 
 
 def before_all(context):
+    """Initialization method that will be executed before the test execution
+
+    :param context: behave context
+    """
     config_files = ConfigFiles()
     config_files.set_config_directory(os.path.join(get_root_path(), 'conf'))
     config_files.set_output_directory(os.path.join(get_root_path(), 'output'))
     config_files.set_config_properties_filenames('properties.cfg', 'local-properties.cfg')
     context.config_files = config_files
     toolium_before_all(context)
+
+
+def before_scenario(context, scenario):
+    """Scenario initialization
+
+    :param context: behave context
+    :param scenario: running scenario
+    """
+    toolium_before_scenario(context, scenario)
+
+
+def after_scenario(context, scenario):
+    """Clean method that will be executed after each scenario
+
+    :param context: behave context
+    :param scenario: running scenario
+    """
+    toolium_after_scenario(context, scenario)
+
+
+def after_all(context):
+    """Clean method that will be executed after all features are finished
+
+    :param context: behave context
+    """
+    toolium_after_all(context)
 
 
 def get_root_path():
