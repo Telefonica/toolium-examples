24877c9a5b2837739c519a3d6a6a8dbfb6a4ddc2
Rubén González Alonso <ruben.gonzalezalonso@telefonica.com>
2016-06-13 17:09:21 +0200

    Fix openshop.io test on android 6.0

--- mobile_behave/pageobjects/initial.py
+++ mobile_behave/pageobjects/initial.py
@@ -16,6 +16,7 @@ from selenium.webdriver.common.by import By
 from mobile_behave.pageobjects.shop import BaseShopPageObject
 from toolium.pageelements import *
 from toolium.pageobjects.mobile_page_object import MobilePageObject
+from selenium.common.exceptions import NoSuchElementException
 
 
 class BaseInitialPageObject(MobilePageObject):
@@ -24,12 +25,13 @@ class BaseInitialPageObject(MobilePageObject):
 
         :returns: initial page object
         """
-        self.accept.wait_until_visible(20)
+        self.country.wait_until_visible(20)
         return self
 
 
 class AndroidInitialPageObject(BaseInitialPageObject):
     country = Button(By.ID, 'bf.io.openshop:id/splash_shop_selection_spinner')
+    english = Button(By.XPATH, '//android.widget.TextView[@text="English"]')
     accept = Button(By.ID, 'bf.io.openshop:id/splash_continue_to_shop_btn')
 
     def go_to_shop(self):
@@ -37,7 +39,13 @@ class AndroidInitialPageObject(BaseInitialPageObject):
 
         :returns: shop page object
         """
-        self.accept.click()
+        try:
+            self.accept.click()
+        except NoSuchElementException:
+            # Language selection is mandatory on Android 6.0
+            self.country.click()
+            self.english.click()
+            self.accept.click()
         return BaseShopPageObject()
 
 
